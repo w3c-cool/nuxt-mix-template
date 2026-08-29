b4394ae4fbe1a8ea0a2fd3f890958f6d26e0a74a
w3c-cool <szo@qq.com>
2026-08-29 20:23:54 +0000

    1

--- server/plugins/powered-by.ts
+++ server/plugins/powered-by.ts
@@ -1,8 +1,10 @@
-// 将 Nuxt 默认的 X-Powered-By 响应头值改为 W3C
-// V8 边缘兼容版：不依赖 Node 的 ctx.event.node.res，仅操作响应头对象
+// 设置 SSR 响应头（V8 边缘兼容版）
+// 不依赖 Node 的 ctx.event.node.res，仅操作响应头对象。
+// 注意：server 头通常由 ESA CDN 在最外层注入，应用层设置可能被平台覆盖。
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:response', (response) => {
     if (!response.headers) response.headers = {}
     response.headers['x-powered-by'] = 'W3C'
+    response.headers['server'] = 'W3C'
   })
 })
